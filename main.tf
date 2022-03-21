443c9884bddd87437e6d2aa04a437e6110da750f
diegotony <diegotony@users.noreply.github.com>
2022-03-21 05:14:52 +0000

    Format Terraform

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 locals {
-  tags = {template = "tf-modules", service = "api-gtwy"}
+  tags = { template = "tf-modules", service = "api-gtwy" }
 }
 
 resource "aws_api_gateway_rest_api" "api_rest" {
-  name        = "api-gtwy-${var.name}"
+  name = "api-gtwy-${var.name}"
   tags = merge(var.tags, local.tags)
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.api_rest.id}"
-  parent_id   = "${aws_api_gateway_rest_api.api_rest.root_resource_id}"
+  rest_api_id = aws_api_gateway_rest_api.api_rest.id
+  parent_id   = aws_api_gateway_rest_api.api_rest.root_resource_id
   path_part   = "{proxy+}"
 
 }
 
 resource "aws_api_gateway_method" "proxy" {
-   rest_api_id   = aws_api_gateway_rest_api.api_rest.id
-   resource_id   = aws_api_gateway_resource.proxy.id
-   http_method   = "ANY"
-   authorization = "NONE"
-    request_parameters = {
+  rest_api_id   = aws_api_gateway_rest_api.api_rest.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+  request_parameters = {
     "method.request.path.proxy" = true
   }
 
--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "lambda_function_names" {
-    type = list(string)
-    description = "(optional) describe your variable"
+  type        = list(string)
+  description = "(optional) describe your variable"
 }
 
 variable "name" {
-    type = string
-    description = "(optional) describe your variable"
+  type        = string
+  description = "(optional) describe your variable"
 }
 
 variable "stage" {
-    type = string
-    description = "(optional) describe your variable"
+  type        = string
+  description = "(optional) describe your variable"
 }
 
 
 variable "tags" {
-    type = map
-    default = {
-        "service" = "api_gateway"
-    }
-    description = "tags"
+  type = map(any)
+  default = {
+    "service" = "api_gateway"
+  }
+  description = "tags"
 }
